86c412ed96f67c7d42a4235a53c4aed776372abc
babyoung666 <1515251178@qq.com>
2022-03-21 13:27:46 +0800

    增加时间格式化，修改时差计算方式

--- src/utils/TimeUtils.js
+++ src/utils/TimeUtils.js
@@ -1,39 +1,39 @@
 // 计算时间
 const TimeUtils = {
     // 计算时差（单位：分）
-    ComputeDiffMinutes(time1,time2){
-        if(time1 == null || time2 == null){
-            return
+    ComputeDiffMinutes(before, after) {
+        if (before == null || before == '' || after == null || after == '') {
+            return 0
         }
 
-        if (time1.search("-") != -1) {
-            time1 = time1.replace(/-/g, '/')
-        }  
-        if (time2.search("-") != -1) {
-            time2 = time2.replace(/-/g, '/')
-        }  
-        let day1 = new Date(time1) 
-        let day2 = new Date(time2) 
-        let between = day1.getMinutes() - day2.day1.getMinutes() 
-        return between
+        if (after.indexOf("-") != -1) {
+            after = after.replace(/-/g, '/')
+        }
+        if (before.indexOf("-") != -1) {
+            before = before.replace(/-/g, '/')
+        }
+        let day1 = new Date(after)
+        let day2 = new Date(before)
+        let diff = (day1.getTime() - day2.getTime()) / (60*1000)
+        //向下取整
+        return Math.ceil(diff)
+        
     },
 
     // 计算出时间差，转成昨天，前天，星期x，年月日
     formatForSimpleView(time) {
-        if(time == null){
-            return
+        if (time == null || time == "") {
+            return ""
         }
 
         let week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
-        if (time == "") {
-            return ""
-        }
-        if (time.search("-") != -1) {
+
+        if (time.indexOf("-") != -1) {
             time = time.replace(/-/g, '/')
         }
         let now = new Date()
         let day = new Date(time)
-        let between = now.getDate() - day.getDate() 
+        let between = now.getDate() - day.getDate()
         if (1 > between && between >= 0) {
             return day.getHours() + ':' + day.getMinutes()
         }
@@ -45,10 +45,10 @@ const TimeUtils = {
         }
         if (7 > between && between >= 3) {
             let weekdif = now.getDay() - day.getDay()
-            if ( now.getDay() == 0 || weekdif > 0) {
+            if (now.getDay() == 0 || weekdif > 0) {
                 return week[day.getDay()]
-            }else{
-                return day.getFullYear() + '/' + (day.getMonth() + 1) + '/' + day.getDate() 
+            } else {
+                return day.getFullYear() + '/' + (day.getMonth() + 1) + '/' + day.getDate()
             }
         }
         if (between >= 7) {
@@ -59,7 +59,7 @@ const TimeUtils = {
     formatForDetial(time) {
         let week = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
 
-        if (time == "") {
+        if (time == null || time == "") {
             return ""
         }
         if (time.search("-") != -1) {
@@ -67,7 +67,7 @@ const TimeUtils = {
         }
         let now = new Date()
         let day = new Date(time)
-        let between = now.getDate() - day.getDate() 
+        let between = now.getDate() - day.getDate()
         if (1 > between && between >= 0) {
             return day.getHours() + ':' + day.getMinutes()
         }
@@ -79,15 +79,33 @@ const TimeUtils = {
         }
         if (7 > between && between >= 3) {
             let weekdif = now.getDay() - day.getDay()
-            if ( now.getDay() == 0 || weekdif > 0) {
+            if (now.getDay() == 0 || weekdif > 0) {
                 return week[day.getDay()]
-            }else{
-                return day.getFullYear() + '年' + (day.getMonth() + 1) + '月' + day.getDate() + '日' + day.getHours() + ':' + day.getMinutes()
+            } else {
+                return day.getFullYear() + '年' + (day.getMonth() + 1) + '月' + day.getDate() + '日　' + day.getHours() + ':' + day.getMinutes()
             }
         }
         if (between >= 7) {
-            return day.getFullYear() + '年' + (day.getMonth() + 1) + '月' + day.getDate() + '日' + day.getHours() + ':' + day.getMinutes()
+            return day.getFullYear() + '年' + (day.getMonth() + 1) + '月' + day.getDate() + '日　' + day.getHours() + ':' + day.getMinutes()
         }
+    },
+
+    dateForMat(fmt, date) { 
+        var o = {
+            "M+": date.getMonth() + 1,                 //月份   
+            "d+": date.getDate(),                    //日   
+            "h+": date.getHours(),                   //小时   
+            "m+": date.getMinutes(),                 //分   
+            "s+": date.getSeconds(),                 //秒   
+            "q+": Math.floor((date.getMonth() + 3) / 3), //季度   
+            "S": date.getMilliseconds()             //毫秒   
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
     }
 }
 export default TimeUtils;
