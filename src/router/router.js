032b2742b1ee102e42b1d97b203d91b27ec222da
babyoung666 <1515251178@qq.com>
2022-03-21 22:41:29 +0800

    删除不需要的router页面

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/views/HelloWorld'
-import home_v1 from '@/views/home_v1'
-import home_v2 from '@/views/home_v2.vue'
 import login from '@/views/login.vue'
 import Register from '@/views/Register.vue'
 import cheat from '@/views/Cheat.vue'
@@ -21,23 +18,6 @@ export default new Router({
       path: '/',
       name: 'cheat',
       component: cheat,
-      
-    },
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/home_v1',
-      name: 'home_v1',
-      component: home_v1,
-      
-    },
-    {
-      path: '/home_v2',
-      name: 'home_v2',
-      component: home_v1,
       
     },
     {
